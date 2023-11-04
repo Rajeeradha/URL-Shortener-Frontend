8ba3ae3abbe4857d0cb0d81df79047b936d273e5
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-11-04 18:46:32 +1100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const env = {
+    // api : "http://localhost:4000/auth",
+    // api1 : "http://localhost:4000/api",
+    // redirect : "http://localhost:4000/api/shortUrl"
+
+    api : "https://url-shortener-d3b4.onrender.com/auth",
+    api1 : "https://url-shortener-d3b4.onrender.com/api",
+    redirect : "https://url-shortener-d3b4.onrender.com/api/shortUrl"
+    
+    
+}
